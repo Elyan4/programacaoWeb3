af50d26f077e9b0bce340f63a60774e491d9ad22
Elyan4 <102760815+Elyan4@users.noreply.github.com>
2022-06-02 21:48:12 -0300

    Add files via upload

---
+++ projeto/php/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+  </head>
+
+  <link rel="stylesheet" href="../css/main.css" />
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+  <body>
+    <header class="menuPrincipal">
+      <main>
+        <div class="logo">
+          <h1>Logotipo</h1>
+        </div>
+        <div class="cadastroLogin">
+          <ul>
+            <li>
+              <a href="../php/subscription.php" target="_blank"
+                >Cadastre-se |</a
+              >
+            </li>
+
+            <li>
+              <a href="../php/login.php" target="_blank"> Login</a>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </header>
+    <main class="col-100 menu-urls">
+      <div class="header2">
+        <div class="menu">
+          <ul>
+            <li><a href="">Destinos |</a></li>
+            <li><a href=""> Hoteis |</a></li>
+            <li><a href=""> Restaurantes |</a></li>
+            <li><a href=""> Pacotes de Viagem</a></li>
+          </ul>
+          <div class="pesquisa">
+          <input type="search" name="pesquisa" id="pesquisa"
+          placeholder="Procure pelo seu destino">
+        </div>
+        </div>
+<div class="col-100">
+  
+<div class="banner">
+  <div><img src=""></div>
+  <div><img src=""></div>
+  <div><img src=""></div>
+</div>
+</div>
+    </main>
+    
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="../slick-1.8.1/slick/slick.min.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
+				
+  </body>
+</html>
